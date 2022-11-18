1643583eb091b23bf043f99389a0a7e32d87c072
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2022-11-18 10:36:40 +0900

    need to fix firebase migration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,7 @@ import type { AppProps } from "next/app";
 import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <MoralisProvider>
-      <Component {...pageProps} />;
-    </MoralisProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
